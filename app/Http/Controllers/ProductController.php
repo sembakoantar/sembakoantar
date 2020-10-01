4444ab2e8a740c6d412bb76d19b26c41a669da9c
SConqueror <fajarhanantyo1234@gmail.com>
2020-10-01 15:46:37 +0700

    au

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
 
 class ProductController extends Controller
 {
